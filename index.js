2b82510c4a83f77db82cd9ba4cdc6d69554969df
DavidMogbeyi <davidm@l5lab.com>
2018-12-15 18:24:04 +0100

    Add home page setup

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const Joi = require('joi');
 const app = express();
 app.use(express.json());
 
+app.get('/', (req, res)=>{
+  res.send('This is the home page');
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, ()=>{
   console.log(`App is listening on port ${port}`)
