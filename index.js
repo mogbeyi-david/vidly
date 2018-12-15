cb38ab2fef1477063d9b9d088f72df8897214b99
DavidMogbeyi <davidm@l5lab.com>
2018-12-15 18:25:24 +0100

    Add endpoint to get all endpoints

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const Joi = require('joi');
 const app = express();
 app.use(express.json());
 
+let genres = [];
+
 app.get('/', (req, res)=>{
   res.send('This is the home page');
+});
+
+app.get('/api/genres', (req, res)=>{
+  res.status(200).send(genres);
 });
 
 const port = process.env.PORT || 3000;
