877ee0594b1cfb7e66de3ffedcda3b755e2f95c1
DavidMogbeyi <davidm@l5lab.com>
2018-12-15 18:40:13 +0100

    Add endpoint to create a new genre

--- index.js
+++ index.js
@@ -14,6 +14,23 @@ app.get('/api/genres', (req, res)=>{
   res.status(200).send(genres);
 });
 
+app.post('/api/genres', (req, res)=>{
+  const schema = Joi.object().keys({
+    genre: Joi.string().required()
+  });
+  const { error } = Joi.validate(req.body, schema);
+  if(error)res.status(400).send(error.details[0].message)
+  const genre = req.body.genre;
+  const id = genres.length + 1;
+  const newGenre = {
+    id: id,
+    genre: genre
+  }
+
+  genres.push(newGenre);
+  res.status(201).send(newGenre);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, ()=>{
   console.log(`App is listening on port ${port}`)
